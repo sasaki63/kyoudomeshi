f8de43ce9429aba1b29fe193cabd93e2ba62c2f9
sasaki63 <cgej0666@mail3.doshisha.ac.jp>
2025-03-22 23:22:44 +0900

    　リリース

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,4 @@
 //= link_tree ../builds
 //= link application.css
 //= link application.js
-//= link raty.js
 //= link users_sessions_new.css
-//= link star-off.png
-//= link star-on.png
-//= link star-half.png
